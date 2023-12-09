432ea5979f293f06646472ebb07c11f669fc469a
Christian Aranas <aranaschristianlouise@gmail.com>
2023-12-10 07:34:20 +0800

    Added secondary color in TailwindCSS config
    This is to be used as a variant of the primary.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
   theme: {
     extend: {},
     colors: {
-      'primary': '#ffc107'
+      'primary': '#ffc107',
+      'secondary': '#ff9800',
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
     }
   },
   plugins: [],
