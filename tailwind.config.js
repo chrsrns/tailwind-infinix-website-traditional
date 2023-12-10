193e87a75e7496f0507ec282ff857032e4c04277
Christian Aranas <aranaschristianlouise@gmail.com>
2023-12-10 08:53:54 +0800

    Added gradient background and animationto the home button in the navbar
    Added another div that has the primary color. This is because gradients don't transition in CSS.
    - So, to transition from a solid color to a gradient, another div is made, transfer the padding to this div, and have the solid color.
    - This new div will then make its background transparent to show the background gradient of its parent.
    Added animation and keyframes to the TailwindCSS config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,28 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        gradientflow: {
+          "0%": {
+            'background-position': '0% 91%'
+          },
+          "50%": {
+            'background-position': '100% 0%'
+          },
+          "100%": {
+            'background-position': '0% 91%'
+          }
+        }
+      },
+      animation: {
+        btngradientflow: 'gradientflow 3s ease infinite'
+      }
+    },
     colors: {
       'primary': '#ffc107',
       'secondary': '#ff9800',
+      'tertiary': '#C1292E',
       'blue': '#1fb6ff',
       'purple': '#7e5bef',
       'pink': '#ff49db',
