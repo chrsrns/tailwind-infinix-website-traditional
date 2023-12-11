6dec50c5fc79091ac16afa233c50354d4aa260d9
Christian Aranas <aranaschristianlouise@gmail.com>
2023-12-11 17:25:30 +0800

    Changed the home navbutton animation to a shine-like animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,26 +10,18 @@ module.exports = {
         'secondary': '#ff9800',
         'tertiary': '#3B3561',
         'orange-1': '#f3a600',
-        'blue': '#1fb6ff',
-        'purple': '#7e5bef',
-        'pink': '#FF4365',
-        'orange': '#ff7849',
-        'green': '#13ce66',
-        'yellow': '#ffc82c',
-        'gray-dark': '#273444',
-        'gray': '#8492a6',
-        'gray-light': '#d3dce6',
       },
       keyframes: {
         gradientflow: {
           "0%": {
-            'background-position': '0% 91%'
+            'background-position': '-100% 0%'
           },
           "50%": {
             'background-position': '100% 0%'
           },
           "100%": {
-            'background-position': '0% 91%'
+            'background-position': '-100% 0%'
+          }
         },
         borderglow: {
           '0%': {
@@ -44,7 +36,7 @@ module.exports = {
         }
       },
       animation: {
-        btngradientflow: 'gradientflow 6s ease infinite',
+        btngradientflow: 'gradientflow 3s ease infinite',
         borderglow: 'borderglow 5s linear infinite'
       },
       backgroundImage: {
