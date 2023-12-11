ad9612a0c56d97f6f97de772726ff12e72442c7b
Christian Aranas <aranaschristianlouise@gmail.com>
2023-12-11 16:51:30 +0800

    Extended the TailwindCSS color palette instead of overwriting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+
+
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        'primary': '#ffc107',
+        'secondary': '#ff9800',
+        'tertiary': '#3B3561',
+        'orange-1': '#f3a600',
+        'blue': '#1fb6ff',
+        'purple': '#7e5bef',
+        'pink': '#FF4365',
+        'orange': '#ff7849',
+        'green': '#13ce66',
+        'yellow': '#ffc82c',
+        'gray-dark': '#273444',
+        'gray': '#8492a6',
+        'gray-light': '#d3dce6',
+      },
       keyframes: {
         gradientflow: {
           "0%": {
@@ -35,21 +52,6 @@ module.exports = {
         'rainbow': 'repeating-linear-gradient(45deg, #ffc107, #ff4365, #ffc107, #ff4365, #ffc107, #ff4365, #ffc107, #ff00c8)'
       }
     },
-    colors: {
-      'primary': '#ffc107',
-      'secondary': '#ff9800',
-      'tertiary': '#3B3561',
-      'orange-1': '#f3a600',
-      'blue': '#1fb6ff',
-      'purple': '#7e5bef',
-      'pink': '#FF4365',
-      'orange': '#ff7849',
-      'green': '#13ce66',
-      'yellow': '#ffc82c',
-      'gray-dark': '#273444',
-      'gray': '#8492a6',
-      'gray-light': '#d3dce6',
-    }
   },
   plugins: [],
 }
