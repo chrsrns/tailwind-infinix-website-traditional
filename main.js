7815a16d57b2f354430a0608e9a746f79f321f30
Christian Aranas <aranaschristianlouise@gmail.com>
2024-02-11 11:39:50 +0800

    Added a library web component
    The library.html is now serialized via WebC using its own webc file.
    Updated main.js to compile the library.html

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
 import { WebC } from "@11ty/webc";
 import fs from "fs";
+
+// Compiling index.html
 let page = new WebC();
-page.defineComponents("components/**.webc");
-page.setInputPath("page.webc");
-let { html, css, js, components } = await page.compile();
-fs.writeFile("./index.html", html, (err) => {
-  if (err) {
-    console.log({ err });
-  }
+const COMPONENTS_DIR = "./components/**.webc";
+page.defineComponents(COMPONENTS_DIR);
+page.setInputPath("./page.webc");
+
+page.compile().then(({ html, css, js, components }) => {
+  fs.writeFile("./index.html", html, (err) => {
+    if (err) {
+      console.log({ err });
+    }
+  });
+});
+
+// Compiling library.html
+let library = new WebC();
+library.defineComponents(COMPONENTS_DIR);
+library.setInputPath("./pages/library.webc");
+
+library.compile().then(({ html, css, js, components }) => {
+  fs.writeFile("./library.html", html, (err) => {
+    if (err) {
+      console.log({ err });
+    }
+  });
 });
