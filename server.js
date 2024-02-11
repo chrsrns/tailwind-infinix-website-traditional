1bdfe8ae6cef5bc6b170e7243d8a95bb75b2d8d6
Christian Aranas <aranaschristianlouise@gmail.com>
2024-02-11 19:33:02 +0800

    Updated server.js to expose necessary files
    Routes are added for ready HTML files.
    Whitelisted static file extensions are passed through.

--- server.js
+++ server.js
@@ -1,30 +1,63 @@
-const http = require("http");
-const fs = require("fs");
-const path = require("path");
+import http from "http";
+import fs from "fs";
+import path from "path";
+import url from "url";
+import { glob } from "glob";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const server = http.createServer((req, res) => {
-  // Handling different routes using a switch statement
-  switch (req.url) {
-    case "/":
-      serveHTMLFile(res, "index.html");
-      break;
-    case "/about":
-      serveHTMLFile(res, "about.html");
-      break;
-    case "/contact":
-      serveHTMLFile(res, "contact.html");
-      break;
-    default:
-      // Handling 404 Not Found for other URLs
+  // Using glob pattern to match routes dynamically
+  if (isStaticFile(req.url)) {
+    // Check if the requested URL matches a media file
+    const mediaFile = getMediaFile(req.url);
+    if (mediaFile) {
+      serveMediaFile(res, mediaFile);
+    } else {
       res.writeHead(404, { "Content-Type": "text/plain" });
       res.end("404 Not Found");
-      break;
+    }
+  } else {
+    switch (req.url) {
+      case "/":
+        serveHTMLFile(res, "index.html");
+        break;
+      case "/about":
+        serveHTMLFile(res, "about.html");
+        break;
+      case "/library":
+        serveHTMLFile(res, "library.html");
+        break;
+      default:
+        // Handling 404 Not Found for other URLs
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("404 Not Found");
+        break;
+    }
   }
 });
 
+// Function to check if the requested URL corresponds to a static file
+function isStaticFile(url) {
+  return url.match(".*\\.(js|css|ico|svg|jpg|png)$");
+}
+
+// Function to match route using glob pattern
+function getRoute(url) {
+  const routes = glob.sync("*.html");
+  const filename = path.basename(url);
+  console.log("path: ", filename);
+  const matchedRoute = routes.find((route) => {
+    console.log("route", path.basename(route));
+    return path.basename(route) === filename;
+  });
+  return matchedRoute ? matchedRoute : null;
+}
+
 // Function to serve HTML files
 function serveHTMLFile(res, filename) {
-  fs.readFile(path.join(__dirname, filename), (err, data) => {
+  fs.readFile(filename, (err, data) => {
     if (err) {
       res.writeHead(500, { "Content-Type": "text/plain" });
       res.end("Internal Server Error");
@@ -35,8 +68,32 @@ function serveHTMLFile(res, filename) {
   });
 }
 
+// Function to match media files
+function getMediaFile(url) {
+  const filePath = path.join(__dirname, url);
+  if (fs.existsSync(filePath)) {
+    return filePath;
+  }
+  return null;
+}
+
+// Function to serve media files
+function serveMediaFile(res, filePath) {
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("Internal Server Error");
+    } else {
+      res.writeHead(200);
+      res.end(data);
+    }
+  });
+}
+
 // Starting the server
 const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+export {};
